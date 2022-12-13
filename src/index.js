21a2630c94ba85710914ee8a9f14364ca7a0c8d2
Madaras Andrei-Iulian <75988913+Mdr-Overflow@users.noreply.github.com>
2022-12-13 03:06:48 +0200

    #1

---
+++ src/index.js
@@ -0,0 +1,116 @@
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+const { auth, requiresAuth } = require('express-openid-connect');
+
+require("dotenv").config();
+
+/**
+ * App Variables
+ */
+
+const env = process.env.NODE_ENV || "development";
+const app = express();
+const port =
+  env === "development" ? process.env.DEV_PORT : process.env.PROD_PORT;
+
+/**
+ *  App Configuration
+ */
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(
+  auth({
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    secret: process.env.SESSION_SECRET,
+    authRequired: false,
+    auth0Logout: true,
+  }),
+);
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.oidc.isAuthenticated();
+  res.locals.activeRoute = req.originalUrl;
+  next();
+});
+
+
+
+
+
+
+
+/**
+ * Routes Definitions
+ */
+
+// > Home
+
+app.get("/", (req, res) => {
+  res.render("home", { activeRoute: req.originalUrl });
+});
+
+// > Profile
+
+app.get('/profile', requiresAuth(),(req, res) => {
+  res.render('profile',{ user: req.oidc.user});
+});
+
+// > External API
+
+app.get("/external-api", (req, res) => {
+  res.render("external-api", { activeRoute: req.originalUrl });
+});
+
+// > Authentication
+
+
+app.get('/sign-up/:page', (req, res) => {
+  const { page } = req.params;
+
+  res.oidc.login({
+    returnTo: page,
+    authorizationParams: {
+      screen_hint: 'signup',
+    },
+  });
+});
+
+app.get('/login/:page', (req, res) => {
+  const { page } = req.params;
+
+  res.oidc.login({
+    returnTo: page,
+  });
+});
+
+app.get('/logout/:page', (req, res) => {
+  const { page } = req.params;
+
+  res.oidc.logout({
+    returnTo: page,
+  });
+});
+
+
+app.get('/extend', (req,res) =>{
+  res.render('extend',{ user: req.oidc.user});
+  });                                     ///CHANGE THIS
+
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
